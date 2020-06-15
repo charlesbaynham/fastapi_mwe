70f4b5faa3814f7cd100be6111e9dcf7d1dd9c42
Charles Baynham <charles.baynham@gmail.com>
2020-06-15 15:41:33 +0100

    Exact dup of issue code

--- test_api.py
+++ test_api.py
@@ -9,10 +9,8 @@ async def read_main():
     return {"msg": "Hello World"}
 
 
-client = TestClient(app)
-
-
 def test_read_main():
+    client = TestClient(app)
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
